d05f0afe748d487565b7f5c2e17c1585810fa86e
Tomas Sedlacek <mail@kedlas.cz>
2018-01-29 12:12:42 +0100

    updated metrics interface

--- src/lib/metrics/IMetrics.ts
+++ src/lib/metrics/IMetrics.ts
@@ -4,7 +4,7 @@ export interface IMetrics {
      * Send metrics message and returns sent message content in promise resolve value
      * or returns the error message in rejection value if sending failed.
      */
-    send(fieldSet: {[key: string]: any}): Promise<string>;
+    send(fieldSet: {[key: string]: any}, includeTime: boolean): Promise<string>;
 
     /**
      * Add new tag to list of tags
